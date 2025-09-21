b632dc5f5bf6de2cf5eba2b54ea9b55d028146d0
Anand raja <meetanandns@gmail.com>
2025-09-21 12:54:42 +0000

    initial commit

---
+++ src/content/blog/missing-content.md
@@ -0,0 +1,5 @@
+---
+title: "This post doesn't have any content"
+description: "This post is purely for testing the table of content, which should not be rendered"
+date: 2023-01-22
+---
---
+++ src/i18n/utils.ts
@@ -0,0 +1,18 @@
+import { ta } from './ta'
+import { en } from './en'
+import { zhCn } from './zhCn'
+import { cs } from './cs'
+import { zhHant } from './zhHant'
+import { config } from "../consts";
+
+const ui = {
+  ta, en, 'zh-cn': zhCn, 'zh-Hant': zhHant, cs
+}
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: string) {
+    return ui[lang][key];
+  }
+}
+
+export const t = useTranslations(config.lang)
+
