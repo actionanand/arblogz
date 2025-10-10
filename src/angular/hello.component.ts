231d452c2379bac38b5c0b1902fa0ac264be87e1
Anand raja <meetanandns@gmail.com>
2025-10-10 18:14:15 +0000

    angular styles

--- src/angular/hello.component.ts
+++ src/angular/hello.component.ts
@@ -1,16 +1,32 @@
-import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  standalone: true,
-  imports: [NgIf],
+  imports: [],
+  styles: [`
+    .btn-toggle {
+      background-color: #04AA6D; /* Green */
+      color: white;
+      padding: 5px 7px;
+      border: none;
+      border-radius: 4px;
+      font-size: 16px;
+      cursor: pointer;
+      transition: background-color 0.3s ease; /* Smooth transition for hover effect */
+    }
+
+    .btn-toggle:hover {
+      background-color: #038C5A; /* Darker green on hover */
+    }
+    `],
   template: `
     <p>Hello from Angular!!</p>
 
-    <p *ngIf="show">{{ helpText }}</p>
+    @if (show) {
+      <p> {{ helpText }} </p>
+    }
 
-    <button (click)="toggle()">Toggle</button>
+    <button (click)="toggle()" class="btn-toggle">Toggle</button>
   `,
 })
 export class HelloComponent {
