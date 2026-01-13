5feeded63d9094f14c36a4db7dab36e790618d72
Anand raja <46064269+actionanand@users.noreply.github.com>
2026-01-13 17:14:55 +0000

    svg refactoring

---
+++ src/data/svg/vastu8.ts
@@ -0,0 +1,228 @@
+export const VASTU_8 = `<svg xmlns="http://www.w3.org/2000/svg" width="880" height="880" viewbox="0 0 880 880">
+<!--  boundary  for 800 x 800  -->
+<rect x="0" y="0" width="800" height="800" fill="none" stroke="#444444"/>
+<text x="810" y="405" fill="#888888" font-size="25" font-family="Nimbus Sans L,sans-serif">East</text>
+
+<text x="10" y="850" fill="black" font-size="40" font-family="Nimbus Sans L,sans-serif">Manduka Mandala - Hindu Temple 64 padas</text>
+<!--  100 increments grid  -->
+<path d="M 0 100 L 800 100" stroke="#444444"/>
+<path d="M 0 200 L 800 200" stroke="#444444"/>
+<path d="M 0 300 L 800 300" stroke="#444444"/>
+<path d="M 0 400 L 800 400" stroke="#444444"/>
+<path d="M 0 500 L 800 500" stroke="#444444"/>
+<path d="M 0 600 L 800 600" stroke="#444444"/>
+<path d="M 0 700 L 800 700" stroke="#444444"/>
+
+<path d="M 100 0 L 100 800" stroke="#444444"/>
+<path d="M 200 0 L 200 800" stroke="#444444"/>
+<path d="M 300 0 L 300 800" stroke="#444444"/>
+<path d="M 400 0 L 400 800" stroke="#444444"/>
+<path d="M 500 0 L 500 800" stroke="#444444"/>
+<path d="M 600 0 L 600 800" stroke="#444444"/>
+<path d="M 700 0 L 700 800" stroke="#444444"/>
+
+<path d="M 0 0 L 800 800" stroke="#444444"/>
+<path d="M 800 0 L 0 800" stroke="#444444"/>
+
+<!--  Paisachika Pada color  -->
+	<rect
+        width="800"
+        height="100"
+        x="0"
+        y="0"
+        style="opacity:0.3;fill:#A3A666;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2939" />
+	<rect
+        width="800"
+        height="100"
+        x="0"
+        y="700"
+        style="opacity:0.3;fill:#A3A666;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2938" />
+	<rect
+        width="100"
+        height="800"
+        x="0"
+        y="0"
+        style="opacity:0.3;fill:#A3A666;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2937" />
+	<rect
+        width="100"
+        height="800"
+        x="700"
+        y="0"
+        style="opacity:0.3;fill:#A3A666;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2936" />
+
+<!--  Manusha Pada color  -->
+	<rect
+        width="600"
+        height="100"
+        x="100"
+        y="100"
+        style="opacity:0.7;fill:#ff9980;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2935" />
+	<rect
+        width="600"
+        height="100"
+        x="100"
+        y="600"
+        style="opacity:0.7;fill:#ff9980;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2934" />
+	<rect
+        width="100"
+        height="600"
+        x="100"
+        y="100"
+        style="opacity:0.7;fill:#ff9980;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2933" />
+	<rect
+        width="100"
+        height="600"
+        x="600"
+        y="100"
+        style="opacity:0.7;fill:#ff9980;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2932" />
+
+<!--  Brahma Pada color  -->
+	<rect
+        width="200"
+        height="200"
+        x="300"
+        y="300"
+        style="opacity:0.85;fill:#0000ff;fill-opacity:1.0000000;fill-rule:nonzero;stroke:none;stroke-width:0.36;
+	stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4.0000000;stroke-dasharray:none;stroke-opacity:1.0000000"
+        id="rect2931" />
+
+<!--  Marmas grid  -->
+<path d="M 200 300 L 200 500" stroke="black" stroke-width="3"/>
+<path d="M 300 200 L 300 600" stroke="black" stroke-width="3"/>
+<path d="M 500 200 L 500 600" stroke="black" stroke-width="3"/>
+<path d="M 600 300 L 600 500" stroke="black" stroke-width="3"/>
+
+<path d="M 300 200 L 500 200" stroke="black" stroke-width="3"/>
+<path d="M 200 300 L 600 300" stroke="black" stroke-width="3"/>
+<path d="M 200 500 L 600 500" stroke="black" stroke-width="3"/>
+<path d="M 300 600 L 500 600" stroke="black" stroke-width="3"/>
+
+<path stroke-dasharray="10,5" d="M 310 190 L 490 190" stroke="black" />
+<path stroke-dasharray="10,5" d="M 490 210 L 590 210" stroke="black" />
+<path stroke-dasharray="10,5" d="M 590 310 L 610 310" stroke="black" />
+<path stroke-dasharray="10,5" d="M 590 490 L 610 490" stroke="black" />
+<path stroke-dasharray="10,5" d="M 490 590 L 590 590" stroke="black" />
+<path stroke-dasharray="10,5" d="M 310 610 L 490 610" stroke="black" />
+<path stroke-dasharray="10,5" d="M 210 590 L 310 590" stroke="black" />
+<path stroke-dasharray="10,5" d="M 190 490 L 210 490" stroke="black" />
+<path stroke-dasharray="10,5" d="M 190 310 L 210 310" stroke="black" />
+<path stroke-dasharray="10,5" d="M 210 210 L 310 210" stroke="black" />
+
+<path stroke-dasharray="10,5" d="M 190 310 L 190 490" stroke="black" />
+<path stroke-dasharray="10,5" d="M 210 490 L 210 590" stroke="black" />
+<path stroke-dasharray="10,5" d="M 310 590 L 310 610" stroke="black" />
+<path stroke-dasharray="10,5" d="M 490 590 L 490 610" stroke="black" />
+<path stroke-dasharray="10,5" d="M 590 490 L 590 590" stroke="black" />
+<path stroke-dasharray="10,5" d="M 610 310 L 610 490" stroke="black" />
+<path stroke-dasharray="10,5" d="M 590 210 L 590 310" stroke="black" />
+<path stroke-dasharray="10,5" d="M 490 190 L 490 210" stroke="black" />
+<path stroke-dasharray="10,5" d="M 310 190 L 310 210" stroke="black" />
+<path stroke-dasharray="10,5" d="M 210 210 L 210 310" stroke="black" />
+
+<!-- Circles -->
+
+<circle cx="400" cy="400" r="5"
+        fill="#ff3300" stroke="#ff3300" stroke-width="1"  />
+
+<circle cx="200" cy="300" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="200" cy="500" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="300" cy="300" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="300" cy="500" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="500" cy="300" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="500" cy="500" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="600" cy="300" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="600" cy="500" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="300" cy="200" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="500" cy="200" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="300" cy="600" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+<circle cx="500" cy="600" r="5"
+        fill="white" stroke="black" stroke-width="1"  />
+
+<!--  Mandala Text  -->
+<text x="303" y="460" fill="white" font-size="30" font-family="Nimbus Sans L,sans-serif">Brahma Padas</text>
+<text x="307" y="560" fill="black" font-size="30" font-family="Nimbus Sans L,sans-serif">Devika Padas</text>
+<text x="295" y="660" fill="black" font-size="30" font-family="Nimbus Sans L,sans-serif">Manusha Padas</text>
+<text x="285" y="760" fill="black" font-size="30" font-family="Nimbus Sans L,sans-serif">Paisachika Padas</text>
+
+<text x="30" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Vayu</text>
+<text x="130" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Naga</text>
+<text x="220" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Mukhya</text>
+<text x="320" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Bhalata</text>
+<text x="430" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Soma</text>
+<text x="530" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Argala</text>
+<text x="630" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Adithi</text>
+<text x="730" y="20" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Dithi</text>
+
+<text x="40" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Mriga</text>
+<text x="120" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Brimga</text>
+<text x="220" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">gTharva</text>
+<text x="320" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Yama</text>
+<text x="410" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">gKshatha</text>
+<text x="510" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Vithada</text>
+<text x="610" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Poosavu</text>
+<text x="720" y="780" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Agni</text>
+
+<text x="10" y="80" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Roga</text>
+<text x="10" y="170" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Shosha</text>
+<text x="10" y="270" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Asura</text>
+<text x="10" y="370" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Varuna</text>
+<text x="5" y="470" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">pDhantan</text>
+<text x="10" y="570" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Surgiva</text>
+<text x="10" y="670" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">dPalakan</text>
+<text x="5" y="725" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">pKKal</text>
+
+<text x="740" y="80" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Isana</text>
+<text x="720" y="170" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">pJanya</text>
+<text x="720" y="270" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Jayanth</text>
+<text x="720" y="370" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Indra</text>
+<text x="720" y="470" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Surya</text>
+<text x="720" y="570" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Sathya</text>
+<text x="720" y="670" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Bhrusa</text>
+<text x="730" y="725" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Antari-</text>
+<text x="747" y="745" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">kshan</text>
+
+<text x="220" y="290" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Rudra</text>
+<text x="240" y="240" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">RJith</text>
+<text x="510" y="240" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Aapa</text>
+<text x="530" y="290" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">AValsa</text>
+<text x="240" y="580" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Indra</text>
+<text x="220" y="525" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">IJith</text>
+<text x="522" y="518" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Savitha</text>
+<text x="505" y="580" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">SThran</text>
+<text x="220" y="360" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Mithra</text>
+<text x="220" y="460" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Mithra</text>
+<text x="510" y="360" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Aaryaka</text>
+<text x="510" y="460" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Aaryaka</text>
+<text x="315" y="595" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Vivaswa</text>
+<text x="415" y="595" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Vivaswa</text>
+<text x="315" y="225" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Bhubrat</text>
+<text x="415" y="225" fill="#888888" font-size="20" font-family="Nimbus Sans L,sans-serif">Bhubrat</text>
+
+</svg>`;
