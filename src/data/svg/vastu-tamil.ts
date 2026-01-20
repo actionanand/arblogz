4140ca1c9b8905e8dbf79c656440fb58b7fc40d4
Anand raja <46064269+actionanand@users.noreply.github.com>
2026-01-20 14:34:18 +0000

    vastu info updated

--- src/data/svg/vastu-tamil.ts
+++ src/data/svg/vastu-tamil.ts
@@ -1,4 +1,4 @@
-export const VASTU_TAMIL = `<svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+export const VASTU_TAMIL = `<svg width="800" height="800" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
   <!-- Define styles -->
   <defs>
     <style>
